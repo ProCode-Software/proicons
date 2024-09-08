c8dca4a3625a9ecfbc21632f05755363946e79bd
ProCode <68168458+ProCode-Software@users.noreply.github.com>
2024-09-08 13:09:17 -0500

    Add icons

--- bin/build.js
+++ bin/build.js
@@ -1,4 +1,4 @@
-import { readdirSync, readFileSync, writeFileSync, unlinkSync } from 'fs'
+import fs from 'fs'
 import path from 'path'
 import { resolveConfig, format } from 'prettier'
 import config from '../src/configs/tags.json' with { type: "json" }
@@ -32,7 +32,9 @@ const inDir = path.join('in')
 const outDir = path.join('icons/svg')
 let variableIcons = []
 
-const files = readdirSync(inDir)
+const files = fs.readdirSync(inDir)
+const newIcons = files.slice()
+
 files.filter(file => file.endsWith('.svg')).forEach(file => {
     const oldPath = path.join(inDir, file);
     if (file.includes(' -Var')) {
@@ -42,12 +44,12 @@ files.filter(file => file.endsWith('.svg')).forEach(file => {
     const newPath = path.join(outDir, newName);
 
     try {
-        let ct = optimize(readFileSync(oldPath, 'utf8'), svgoConfig)
+        let ct = optimize(fs.readFileSync(oldPath, 'utf8'), svgoConfig)
         strokeColors.forEach(color => {
             ct = ct.replaceAll(color, 'currentColor')
         })
-        writeFileSync(newPath, ct)
-        unlinkSync(oldPath)
+        fs.writeFileSync(newPath, ct)
+        fs.unlinkSync(oldPath)
     } catch (error) {
         console.error(chalk.red(`Error moving file ${file}:`, error));
     }
@@ -58,14 +60,14 @@ console.log(chalk.green('Done renaming files!'))
 // Build PNGs
 const buildPngs = async () => {
     const pngSizes = [24, 72, 120]
-    const svgFiles = readdirSync(outDir)
+    const svgFiles = fs.readdirSync(outDir)
         .filter(file => file.endsWith('.svg'))
 
     const progresBar = new progress('  Build PNGs [:bar] :item :percent :etas', {
         complete: '=',
         incomplete: ' ',
         width: 20,
-        total: svgFiles.length,
+        total: svgFiles.length * 3 * 2,
     })
 
     for (const file of svgFiles) {
@@ -75,10 +77,14 @@ const buildPngs = async () => {
             const newFolder = path.join(`icons/png${scale == 1 ? '' : `@${scale}x`}`)
 
             for (const color of colors) {
-                const fileStr = readFileSync(path.join(outDir, file), 'utf-8')
+                const newColorFolder = path.join(newFolder, color)
+
+                if (!fs.existsSync(newColorFolder)) fs.mkdirSync(newColorFolder, { recursive: true })
+
+                const fileStr = fs.readFileSync(path.join(outDir, file), 'utf-8')
                     .replaceAll('currentColor', color)
 
-                const newFilePath = path.join(newFolder, `${file.slice(0, -4)}.png`)
+                const newFilePath = path.join(newFolder, color, `${file.slice(0, -4)}.png`)
 
                 progresBar.tick(1, {
                     item: newFilePath
@@ -98,14 +104,28 @@ const buildPngs = async () => {
 }
 buildPngs()
 
-// Build SVG list
+// Build SVG list and lockfile
 const dict = {}
+const lockfile = JSON.parse(fs.readFileSync(path.join('icons/icons.lock.json'), 'utf8')) || []
+
 Object.keys(config).forEach(friendlyName => {
     const name = rename.camelCase(friendlyName.trimEnd())
     const fn = rename.kebabCase(friendlyName.trimEnd())
 
     try {
-        dict[name] = readFileSync(path.join(outDir, `${fn}.svg`), 'utf8')
+        dict[name] = fs.readFileSync(path.join(outDir, `${fn}.svg`), 'utf8')
+
+        if (!lockfile.some((z) => z.name == friendlyName)) {
+            const lfItem = {
+                name: friendlyName,
+                added: JSON.parse(
+                    fs.readFileSync('../package.json', 'utf-8')).version
+            }
+            lockfile.push(lfItem)
+        } else if (newIcons.indexOf(`${friendlyName}.svg`)) {
+            lockfile.find((z) => z.name == friendlyName).updated = JSON.parse(
+                fs.readFileSync('../package.json', 'utf-8')).version
+        }
     } catch (error) {
         console.error(chalk.red(`Error reading file ${fn}.svg:`, error));
     }
@@ -114,19 +134,26 @@ buildSvgList()
 
 async function buildSvgList() {
     try {
-        const options = await resolveConfig(path.join(__dirname, '../.prettierrc'))
-        options.parser = 'json'
-        const formatted = await format(JSON.stringify(dict), options)
+        for (const doc of [dict, lockfile]) {
+            const options = await resolveConfig(path.join(__dirname, '../.prettierrc'))
+            options.parser = 'json'
+            const formatted = await format(JSON.stringify(doc), options)
+            const location = doc == dict ? 'src/configs/icons.json' : 'icons/icons.lock.json'
 
-        writeFileSync(path.join('src/configs/icons.json'), formatted)
+            fs.writeFileSync(path.join(location), formatted)
+        }
 
         console.log('Done building SVG list!')
         console.log(chalk.green('Build complete!'));
 
-        if (variableIcons.length > 0) {
-            console.log(chalk.cyan('Variable icons:', variableIcons))
+        if (newIcons > 0) {
+            console.log(chalk.cyan('New icons:', newIcons))
+
+            if (variableIcons.length > 0) {
+                console.log(chalk.cyan('\tVariable:', variableIcons))
+            }
         } else {
-            console.log(chalk.green('No newly added variable icons!'))
+            console.log(chalk.green('No newly added icons!'))
         }
     } catch (error) {
         console.error(chalk.red('Error writing icons.json:', error));
