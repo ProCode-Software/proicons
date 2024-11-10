145bd76d3c369547d856ebfacf998529005a6ff1
ProCode Software <68168458+ProCode-Software@users.noreply.github.com>
2024-11-09 18:07:55 -0600

    Remove variable icons

---
+++ remove.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Define the folders and items to remove
+folders=("/black" "/white" "@3x/black" "@3x/white" "@5x/black" "@5x/white")
+items=("comment-multiple--var" "copy--var" "tag-multiple--var" "chrome-restore--var" "window-multiple--var" "bookmark-multiple--var" "add-square-multiple--var")
+
+# Loop through each folder
+for folder in "${folders[@]}"; do
+    echo "Processing folder: icons/$folder"
+    
+    # Determine the file extension based on folder name
+    if [[ $folder == "svg" ]]; then
+        ext="svg"
+    else
+        ext="png"
+    fi
+    
+    # Loop through each item to remove
+    for item in "${items[@]}"; do
+        file="icons/png$folder/$item.$ext"
+        if [ -f "$file" ]; then
+            rm "$file"
+            echo "Removed: $file"
+        else
+            echo "File not found: $file"
+        fi
+    done
+done
