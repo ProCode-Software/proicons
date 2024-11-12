2ed2336ad90962ed55e002aa273469a05d16d6d2
ProCode <68168458+ProCode-Software@users.noreply.github.com>
2024-11-11 18:59:11 -0600

    Edit Roblox package

--- packages/proicons-roblox/bin/buildIcons.js
+++ packages/proicons-roblox/bin/buildIcons.js
@@ -7,20 +7,27 @@ import { writeFileSync } from 'fs'
 
 const __dirname = import.meta.dirname
 
+const includedBrands = ['Roblox'] // All brand icons removed by default
+const removedKeyWords = ['die', 'kill', 'death', 'x premium', 'twitter blue'] // For tags
+
 const outputFile = resolve(__dirname, '../src/icons.luau')
 
-const entries = Object.entries(icons).map(
-    ([friendlyName, { description, category }]) => {
-        if (category == 'Logos & Brands') return
+const entries = Object.entries(icons)
+    .map(([friendlyName, { description, category }]) => {
+        if (category == 'Logos & Brands' && !includedBrands.includes(friendlyName))
+            return
         const hasTags = description.length
-        const tagStrings = description.split(',').map(d => JSON.stringify(d.trim()))
+        const tagStrings = description
+            .split(',')
+            .filter(d => !removedKeyWords.includes(d.trim().toLowerCase()))
+            .map(d => JSON.stringify(d.trim()))
 
         // TODO: add icon IDs
         return `${pascalCase(friendlyName)} = {
     category = ${JSON.stringify(category)}${hasTags ? `,\n    tags = { ${tagStrings.join(', ')} }` : ''}
 }`.replaceAll('\n', '\n    ')
-    }
-)
+    })
+    .filter(m => m)
 
 const template = `return {
     ${entries.join(',\n    ')}
