1e699e92a090f4659b8bddc0a11925fcc1f86f27
ProCode <68168458+ProCode-Software@users.noreply.github.com>
2024-11-12 20:09:07 +0000

    Edit Vue package

---
+++ packages/proicons-vue/src/createIcon.ts
@@ -0,0 +1,57 @@
+import { type FunctionalComponent, h } from 'vue'
+import { kebabCase, kebabToPascalCase, pascalToCamelCase } from '../../../src/rename'
+import { convertNodesWithConfig } from '../../../src/renderNodes'
+import { type ProIconAttributes } from './types'
+
+export type IconNode = [string, Record<string, string>, IconNode[]]
+
+export const convertNodes = (n: IconNode[]) => {
+    return !n.length
+        ? []
+        : n.map(([tag, attrs, children]) => {
+            return h(
+                tag,
+                Object.fromEntries(
+                    Object.entries(attrs).map(([k, v]) => [
+                        pascalToCamelCase(kebabToPascalCase(k)),
+                        v,
+                    ])
+                ),
+                convertNodes(children)
+            )
+        })
+}
+
+export function createIcon(
+    {
+        name,
+        deprecated,
+        alternative,
+    }: { name: string; deprecated?: boolean; alternative?: string },
+    nodes: IconNode[]
+): FunctionalComponent<ProIconAttributes> {
+    if (deprecated) {
+        console.warn(`Icon ${name} is deprecated. Use ${alternative} instead.`)
+    }
+
+    return (props, { slots }) => {
+        return h(
+            'svg',
+            {
+                width: props?.size ?? 24,
+                height: props?.size ?? 24,
+                xmlns: 'http://www.w3.org/2000/svg',
+                viewBox: '0 0 24 24',
+                fill: 'none',
+                class: ['proicon', props.class].flat(),
+                'data-proicon-id': kebabCase(n``ame),
+                ...props,
+            },
+            [
+                // @ts-ignore
+                ...(convertNodes(convertNodesWithConfig(nodes, props)) ?? []),
+                ...(slots.default ? slots.default() : [])
+            ]
+        )
+    }
+}
---
+++ packages/proicons-vue/src/types.ts
@@ -0,0 +1,22 @@
+import { type SVGAttributes } from 'vue'
+
+export interface ProIconsOptions {
+    /** Determines the color of the icons. Defaults to `currentColor`. */
+    color?: string
+    /** Determines the default stroke width of the icon. Defaults to `1.5`. This only works on SVG elements with existing strokes; add `outline` for this property to affect such elements. */
+    strokeWidth?: number
+    /** Apply strokes to filled SVG elements, such as circles, by the value of `strokeWidth` with `1.5` (default stroke value) subtracted, if `width` is set to a value above `1.5`. Defaults to `false`
+     * @example If `strokeWidth` is set to `2`, filled SVG elements will have an additional `0.5` pixel stroke
+     */
+    outline?: boolean
+    /** Defaults to `round` */
+    caps?: 'round' | 'square' | 'butt'
+    /** Defaults to `round` */
+    join?: 'round' | 'miter' | 'bevel'
+    /** Determines the corner radius of SVG elements. Does not apply to all rounded elements. */
+    cornerRadius?: number
+    /** Determines the size of the icon in pixels. Defaults to `24` */
+    size?: number
+}
+
+export type ProIconAttributes = Partial<SVGAttributes> & ProIconsOptions
