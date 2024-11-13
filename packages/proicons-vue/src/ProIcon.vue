087b54a93a72f1c183523e5bb1e1a43cb6994c35
ProCode <68168458+ProCode-Software@users.noreply.github.com>
2024-11-13 19:11:13 +0000

    Edit Vue package

--- packages/proicons-vue/src/ProIcon.vue
+++ packages/proicons-vue/src/ProIcon.vue
@@ -6,11 +6,12 @@ import * as icons from './icons'
 type IconEnum<T extends string> = T extends `${infer Base}Icon` ? Base : T
 type IconProp = IconEnum<keyof typeof icons> | (string & {})
 
-const props = defineProps<ProIconAttributes & { icon: IconProp }>()
+const {icon: iconProp, ...props} = defineProps<ProIconAttributes & { icon: IconProp }>()
 
 const count = ref(0)
 </script>
 
 <template>
+    
     <button type="button" @click="count++">{{ icon }} count is {{ count }}</button>
 </template>
