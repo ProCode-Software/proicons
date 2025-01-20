3aa8f9f2da0ee803a5cf25dd790411f725588b58
ProCode Software <68168458+ProCode-Software@users.noreply.github.com>
2025-01-20 13:11:40 -0600

    Add icon previews to changelog

--- bin/generateChangelog.js
+++ bin/generateChangelog.js
@@ -2,6 +2,7 @@ import { readFileSync, writeFileSync } from 'fs'
 import { resolve } from 'path'
 import lockfile from '../icons/icons.lock.json' with { type: 'json' }
 import pkg from '../package.json' with { type: 'json' }
+import { kebabCase } from './helpers/rename.js'
 const { version } = pkg
 
 const shouldWrite = process.argv.includes('--write') || process.argv.includes('-w')
@@ -19,12 +20,14 @@ export function generateChangelog() {
         }
     }
 
+    const toListItem = name => `- ![](https://raw.githubusercontent.com/ProCode-Software/proicons/main/icons/png/white/${kebabCase(name)}.png#gh-dark-mode-only)![](https://raw.githubusercontent.com/ProCode-Software/proicons/main/icons/png/black/${kebabCase(name)}.png#gh-light-mode-only) ${name}`
+
     let changelog = ``
     if (newIcons.length) {
-        changelog += `## New icons\n${newIcons.map(s => `- ${s}`).join('\n')}`
+        changelog += `## New icons\n${newIcons.map(toListItem).join('\n')}`
     }
     if (updatedIcons.length) {
-        changelog += `\n\n## Updated icons\n${updatedIcons.map(s => `- ${s}`).join('\n')}`
+        changelog += `\n\n## Updated icons\n${updatedIcons.map(toListItem).join('\n')}`
     }
 
     console.log(changelog)
