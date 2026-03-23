ea14d68efbd03f429e128fbe190f969a71362956
ProCode Software <68168458+ProCode-Software@users.noreply.github.com>
2026-03-22 19:01:03 -0500

    React: Migrate icon syntax to React 19

---
+++ packages/proicons-react/src/createIcon.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react'
+import { kebabCase, kebabToCamelCase } from '@proicons/shared'
+import { convertNodesWithConfig } from '@proicons/shared'
+import { ProIconAttributes } from './types'
+
+export type IconNode = [string, Record<string, string>, IconNode[]]
+
+export const convertNodes = (n: IconNode[]) => {
+    return !n.length
+        ? null
+        : n.map(([tag, attrs, children]) => {
+            return React.createElement(
+                tag,
+                Object.fromEntries(
+                    Object.entries(attrs).map(([k, v]) => [kebabToCamelCase(k), v])
+                ),
+                convertNodes(children)
+            )
+        })
+}
+
+export type IconComponent = React.FunctionComponent<ProIconAttributes>
+
+export function createIcon(
+    {
+        name,
+        deprecated,
+        alternative,
+    }: { name: string; deprecated?: boolean; alternative?: string },
+    nodes: IconNode[]
+): IconComponent {
+    if (deprecated)
+        console.warn(`Icon ${name} is deprecated. Use ${alternative} instead.`)
+
+    const Icon: IconComponent = ({ ref, ...props }: ProIconAttributes) => (
+        <svg
+            ref={ref}
+            width={props?.size ?? 24}
+            height={props?.size ?? 24}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            {...props}
+            className={((props?.className ?? '') + ' proicon').trim()}
+            data-proicon-id={kebabCase(name)}
+        >
+            {convertNodes(convertNodesWithConfig(nodes, props) as IconNode[])}
+        </svg>
+    )
+    Icon.displayName = name
+    return Icon
+}
--- packages/proicons-react/test/App.tsx
+++ packages/proicons-react/test/App.tsx
@@ -1,19 +1,20 @@
 import * as React from 'react'
 import { createRoot } from 'react-dom/client'
-import { ProIcon } from '../src/ProIcon.ts'
-import { AddIcon, TvIcon } from '../src/icons.ts'
+import { ProIcon } from '../src/ProIcon.tsx'
+import { AddIcon, TvIcon, BrainIcon } from '../src/icons.ts'
 
 function App() {
+    const [size, setSize] = React.useState(48)
+    const randomIcons = ['Photo', 'Search', 'Cheese', 'Toast', 'Save']
+    const getRandomIcon = () =>
+        randomIcons[Math.floor(Math.random() * randomIcons.length)]
+    const [icon, setIcon] = React.useState(getRandomIcon())
+
     return (
         <>
             <h1>ProIcons React Test</h1>
             <ProIcon icon="Add Square Multiple" size={32} className="myClass" />
-            <AddIcon
-                size={32}
-                color="red"
-                strokeWidth={2}
-                className="myIconClassAdd"
-            />
+            <AddIcon size={32} color="red" strokeWidth={2} className="myIconClassAdd" />
             {/* Test for https://github.com/ProCode-Software/proicons/issues/5 */}
             <div style={{ color: 'green' }}>
                 <AddIcon color="red" /> {/* red ✓ */}
@@ -23,6 +24,21 @@ function App() {
                 <AddIcon /> {/* red x */}
                 <TvIcon /> {/* blue ✓ */}
             </div>
+            <h2>Dynamic icons</h2>
+            Size:{' '}
+            <input
+                type="range"
+                value={size}
+                onChange={e => setSize(+e.target.value)}
+                min="24"
+                max="72"
+            />
+            <br />
+            <ProIcon icon="Search Cancel" size={size} />
+            <BrainIcon size={size} />
+            <br />
+            <ProIcon icon={icon} size={size} />
+            <button onClick={() => setIcon(getRandomIcon())}>Random Icon</button>
         </>
     )
 }
